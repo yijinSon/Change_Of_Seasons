d9bb8d492d771c149e74ba3dfd2c87203f331ccb
yijinSon <93176748+yijinSon@users.noreply.github.com>
2021-11-01 23:55:23 +0900

    made snow fadeout

--- snow.js
+++ snow.js
@@ -6,15 +6,19 @@ class Snow{
     this.acc = createVector(0,random(0.03,0.08));
     this.size = random (4,6);
     this.groundCheck = false;
+    this.alpha = 0;
   }
-  
+
   update(){
     this.vel.add(this.acc);
     this.pos.add(this.vel);
+    if (frameCount > 700) {
+      this.alpha += 1;
+    }
   }
 
   display(){
-    fill (255,150);
+    fill (255,150-this.alpha);
     noStroke();
     ellipseMode(CENTER);
     ellipse(this.pos.x, this.pos.y, this.size*2, this.size*2);
