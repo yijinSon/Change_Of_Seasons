bf9aa8ae144e47acd343a150b53cdf14677e8be0
yijinSon <93176748+yijinSon@users.noreply.github.com>
2021-11-01 23:38:50 +0900

    change canvas size

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var winterIsCome = false;
 var flowers = [];
 
 function setup() {
-  createCanvas(300, 500);
+  createCanvas(600, 1000);
   for (let i=0 ; i<10 ; i++){
     snowy[i] = new Snow();
   }
@@ -14,33 +14,33 @@ function draw() {
 
     background(0);
     winterToSpring();
-  
+
   for (let i=0 ; i<snowy.length ; i++){
     snowy[i].update();
     snowy[i].display();
     snowy[i].ground();
   }
-  
+
   pileup();
-  
+
     for (var i=0 ; i<flowers.length ; i++){
     flowers[i].update();
     flowers[i].display();
   }
-  
-    textSize (10);
+
+    textSize (20);
     textAlign (CENTER);
     fill (255, 200);
   if (winterIsCome == false){
-    text ('If you want to melt the snow, press the space bar', width/2, 30);
+    text ('If you want to melt the snow, press the space bar', width/2, 40);
   } else {
-    text ('Winter has gone and spring has come', width/2, 30);
-    text ('Click on the mouse', width/2, 50);
+    text ('Winter has gone and spring has come', width/2, 40);
+    text ('Click on the mouse', width/2, 70);
   }
 }
 
 function mousePressed(){
-  
+
   if (winterIsCome == true){
     var f = new Flower(mouseX, mouseY);
     flowers.push(f);
--- snow.js
+++ snow.js
@@ -1,10 +1,10 @@
 class Snow{
-  
+
   constructor(){
     this.pos = createVector(random(width),-50);
     this.vel = createVector(0,0);
     this.acc = createVector(0,random(0.03,0.08));
-    this.size = random (2,4);
+    this.size = random (4,6);
     this.groundCheck = false;
   }
   
@@ -12,14 +12,14 @@ class Snow{
     this.vel.add(this.acc);
     this.pos.add(this.vel);
   }
-  
+
   display(){
     fill (255,150);
     noStroke();
     ellipseMode(CENTER);
-    ellipse(this.pos.x, this.pos.y, this.size*2, this.size*2);    
+    ellipse(this.pos.x, this.pos.y, this.size*2, this.size*2);
   }
-  
+
     ground(){
     if (this.pos.y > height-this.size*2){
       this.vel.y = 0;
